d38cc91713102379ee93d6906ad44e344615d2db
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 12:20:04 +0600

    proop

--- property.js
+++ property.js
@@ -1,18 +1,18 @@
-const sawon = {
-    name: "sawon ahmed",
-    age: 18,
-    stutus: "unmaried",
-    vergin: true,
-    income: 250000,
-    relation: {
-        days: "many",
-        year: 4,
-        physical: "no",
-    }
-}
+// const sawon = {
+//     name: "sawon ahmed",
+//     age: 18,
+//     stutus: "unmaried",
+//     vergin: true,
+//     income: 250000,
+//     relation: {
+//         days: "many",
+//         year: 4,
+//         physical: "no",
+//     }
+// }
 // const sallary = sawon.income;
 // console.log(sallary)
 
 // console.log(sawon["age"])
-const man = sawon["age"]
-console.log(man)
+// const man = sawon["age"]
+// console.log(man)
--- reverse.js
+++ reverse.js
@@ -1,2 +1,2 @@
-const sen = "i am the best hater of messi because of chirstanity"
-console.log(sen.split(" ").reverse(" ").join(" "))
+// const sen = "i am the best hater of messi because of chirstanity"
+// console.log(sen.split(" ").reverse(" ").join(" "))
