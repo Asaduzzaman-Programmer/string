a88fbd36293fbab7bfd2f6f2770d711bd8e000b7
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 11:26:07 +0600

    fisrt

---
+++ string.js
@@ -0,0 +1,17 @@
+// const subject = "Chemistry";
+// const book = "chemistry";
+
+// if( subject.toLocaleLowerCase === book.toLocaleLowerCase){
+//     console.log("Your the funny man")
+// }
+// else{
+//     console.log("hudai hudai prista ultai felmu")
+// }
+
+// const address = "bandarban";
+// const part = address.slice(3, 8)
+// console.log(part)
+
+// const address = "bandarban";
+// const paste = address.slice(1, 8);
+// console.log(paste);
