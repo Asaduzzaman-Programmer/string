e58b2bf30fa10ac2351a7ebbff7ea9a41f5768dc
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 12:01:41 +0600

    property

---
+++ property.js
@@ -0,0 +1,14 @@
+const sawon = {
+    name: "sawon ahmed",
+    age: 18,
+    stutus: "unmaried",
+    vergin: true,
+    income: 250000,
+    relation: {
+        days: "many",
+        year: 4,
+        physical: "no",
+    }
+}
+const sallary = sawon.income;
+console.log(sallary)
