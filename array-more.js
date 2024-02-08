1c138a59769fbc15fc863a726de246c13638cdbe
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 13:58:41 +0600

    more

---
+++ array-more.js
@@ -0,0 +1,34 @@
+// const friends = [ "mask", "bill", "mark", "waren"]
+// for (const bal of friends){
+//     // console.log(bal)
+// }
+
+// for ( let i = 0; i < friends.length; i++){
+//     console.log(i);
+//     console.log(friends[i])
+// }
+
+// const bal = [15,5458,584,5,4,4,54,4,584,58,5,4]
+
+// for (let i = 0; i < bal.length; i++){
+  
+//     console.log(bal[i])
+// }
+
+
+const chap = [1245,545445,1465,1,544,459];
+
+for (let i = 0; i < chap.length; i++){
+    console.log(chap[i])
+}
+
+
+
+
+
+
+
+
+
+
+
