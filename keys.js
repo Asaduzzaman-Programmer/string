32a838d1a046077777ddd8005815bd24c9f0f3fb
asad <asaduzzamanprogrammer@gmail.com>
2024-02-08 12:59:33 +0600

    keys

---
+++ keys.js
@@ -0,0 +1,11 @@
+const computer = {
+    brand: "asus",
+    processor: "intel",
+    ram: 8,
+    price: 25000, 
+}
+
+// const keys = Object.keys(computer)
+// console.log(keys)
+const value = Object.values(computer);
+console.log(value)
